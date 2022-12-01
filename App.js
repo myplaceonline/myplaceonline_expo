c28464980666b253a23cc849e9fc9787615e85a5
Kevin G <kevin@myplaceonline.com>
2022-12-01 14:05:52 -0600

    Use WebView

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://myplaceonline.com/" }}
+    />
+  );
+}
